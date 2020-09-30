26cb3b17b8b6ba253369aa871053739d42007692
Rohan Sharma <notrohan@Rohans-MacBook-Pro.local>
2020-09-30 12:58:37 +0530

    implemented transaction pool

--- app/pubsub.js
+++ app/pubsub.js
@@ -2,12 +2,14 @@ const redis = require('redis');
 
 const CHANNELS = {
   TEST: 'TEST',
-  BLOCKCHAIN: 'BLOCKCHAIN'
+  BLOCKCHAIN: 'BLOCKCHAIN',
+  TRANSACTION: 'TRANSACTION'
 };
 
 class PubSub {
-  constructor({ blockchain }) {
+  constructor({ blockchain, transactionPool }) {
     this.blockchain = blockchain;
+    this.transactionPool = transactionPool;
 
     this.publisher = redis.createClient();
     this.subscriber = redis.createClient();
@@ -38,13 +40,26 @@ class PubSub {
     })
   }
 
+  broadcastTransaction(transaction) {
+    this.publish({
+      channel: CHANNELS.TRANSACTION,
+      message: JSON.stringify(transaction)
+    })
+  }
+
   handleMessage(channel, message) {
     console.log(`Message recieved channel:${channel}, message ${message}`);
 
     const parsedMessage = JSON.parse(message);
 
-    if(channel === CHANNELS.BLOCKCHAIN) {
-      this.blockchain.replaceChain(parsedMessage);
+    switch (channel) {
+      case CHANNELS.BLOCKCHAIN:
+        this.blockchain.replaceChain(parsedMessage);
+        break;
+      case CHANNELS.TRANSACTION:
+        this.transactionPool.setTransaction(parsedMessage);
+      default:
+        return;
     }
   }
 }
