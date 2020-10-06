15ef918584efc21704616a78a752a65ba2d86f45
Rohan Sharma <notrohan@Rohans-MacBook-Pro.local>
2020-10-06 18:18:06 +0530

    added valid blockchain data tests

--- blockchain/index.js
+++ blockchain/index.js
@@ -1,5 +1,8 @@
 const Block = require('./block');
 const { cryptoHash } = require('../util');
+const Wallet = require('../wallet');
+const { REWARD_INPUT, MINING_REWARD } = require('../config');
+const Transaction = require("../wallet/transaction");
 
 class Blockchain {
   constructor() {
@@ -13,6 +16,54 @@ class Blockchain {
     });
 
     this.chain.push(newBlock);
+  }
+
+  validTransactionData({ chain }) {
+    for(let i=1; i<chain.length; i++) {
+      const block = chain[i];
+      const transactionSet = new Set();
+      let rewardTransactionCount = 0;
+
+      for(let transaction of block.data) {
+        if(transaction.input.address === REWARD_INPUT.address) { 
+          rewardTransactionCount += 1;
+
+          if(rewardTransactionCount > 1) {
+            console.error('Miner rewards exceed limit');
+            return false;
+          }
+
+          if(Object.values(transaction.outputMap)[0] !== MINING_REWARD) {
+            console.error('Miner reward amount is invalid');
+            return false;
+          }
+        } else {
+          if(!Transaction.validTransaction(transaction)) {
+            console.error('Invalid Transaction');
+            return false;
+          }
+
+          const trueBalance = Wallet.calculateBalance({
+            chain: this.chain,
+            address: transaction.input.address
+          });
+
+          if(transaction.input.amount !== trueBalance) {
+            console.error('Invalid input amount');
+            return false;
+          }
+
+          if(transactionSet.has(transaction)) {
+            console.error('An identical transaction appears more than once in the block');
+            return false;
+          } else {
+            transactionSet.add(transaction);
+          }
+        }
+      }
+    }
+
+    return true;
   }
 
   isValidChain(chain) {
@@ -35,7 +86,7 @@ class Blockchain {
     return true;
   }
 
-  replaceChain(chain, onSuccess) {
+  replaceChain(chain, validateTransactions, onSuccess) {
     if (chain.length <= this.chain.length) {
       console.error('The incoming chain must be longer')
       return;
@@ -43,6 +94,11 @@ class Blockchain {
 
     if(!this.isValidChain(chain)) {
       console.error('The incoming chain must be valid')
+      return;
+    }
+
+    if(validateTransactions && !this.validTransactionData({ chain })) {
+      console.error('The incoming chain has invalid data');
       return;
     }
 
