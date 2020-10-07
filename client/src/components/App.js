59a559eb6d4425db0c467c66bbd8df4f13c6acda
Rohan Sharma <notrohan@Rohans-MacBook-Pro.local>
2020-10-07 22:48:32 +0530

    completed frontend part of application

---
+++ client/src/components/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.jpeg';
+
+class App extends Component{
+  state = {
+    walletInfo: {}
+  }
+
+  componentDidMount() {
+    fetch(`${document.location.origin}/api/wallet-info`)
+    .then((response) => response.json())
+    .then(json => this.setState({ walletInfo: json }))
+  }
+
+  render() {
+    const { address, balance } = this.state.walletInfo;
+    return (
+      <div className='App'>
+        <img className='logo' src={logo}></img>
+        <br />
+        <div>Welcome to the NotBitcoin</div>
+        <br />
+        <div>
+          <Link to='/Blocks'>Blocks</Link>
+        </div>
+        <div>
+          <Link to='/conduct-transaction'>Conduct a transaction</Link>
+        </div>
+        <div>
+          <Link to='/transaction-pool'>Transaction Pool</Link>
+        </div>
+        <br />
+        <div className='walletInfo'>
+          <div>Address: {address}</div>
+          <div>Balance: {balance}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
